a00a05e6c661966355a9f8a5e609d857a5980eca
HercMakesThings <hercmakesthings@gmail.com>
2026-03-04 18:36:54 -0500

    air dodge ability initial commit

--- scripts/airdodge_ability.gd
+++ scripts/airdodge_ability.gd
@@ -1,1 +1,13 @@
-extends Ability
+class_name Airdodge extends Ability
+
+@onready var body: PlayerNew = $"../.."
+
+@export var air_dodge_count: int = 1
+@export var air_dodge_length: int = 30
+var air_dodge: int
+
+func _ready() -> void:
+	air_dodge = air_dodge_count
+
+func tick_ability(input: InputGameComponent, movement: MovementComponent) -> void:
+	pass
